462208dba7565a353c3b4aacd23bc13ce7230384
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-03-04 15:44:43 +0100

    Initial commit

---
+++ src/models/Student.ts
@@ -0,0 +1,9 @@
+﻿export type Grade = number | "A" | "B" | "C" | "D" | "E" | "F" | undefined
+export type Student =
+{
+    firstName: string
+    lastName: string
+    age: number
+    grades: Grade[]
+}
+
